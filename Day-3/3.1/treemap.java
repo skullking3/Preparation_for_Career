c41c5fab627e5934c442384572e3f69ef87d929b
Vishal Kumar <callvk3@gmail.com>
2025-08-21 21:26:16 +0530

    Add files via upload

---
+++ Day-3/3.1/treemap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class treemap {
+    public static void main(String[] args) {
+
+        
+        TreeMap<Integer,String> nw = new TreeMap<>();
+
+
+        nw.put(1,"Vishal"); 
+        nw.put(3,"Aditya"); 
+        nw.put(4,"Mohit"); 
+        nw.put(2,"Ravi"); 
+        nw.put(6,"Ram");
+        nw.put(5,"Saurabh");
+
+        System.out.println(nw);
+    }
+    
+}
+    
+
